596bc78a42bb8963be57d1dfae12eea31bf0b255
Caleb Severn <calsev@gmail.com>
2019-08-09 16:30:41 -0800

    Added platforms to metadata

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,16 @@ issues_url "#{git_url}/issues" if respond_to?(:issues_url)
 
 version '1.0.0'
 
+supports 'ubuntu', '>= 14.0'
+supports 'debian', '>= 8.0'
+supports 'redhat', '>= 6.0'
+supports 'centos', '>= 6.0'
+supports 'oracle', '>= 6.0'
+supports 'fedora'
+supports 'amazon'
+supports 'suse'
+# supports 'opensuse'
+
 chef_version '>= 14.0' if respond_to?(:chef_version)
 
 depends 'checksum_file'
